9482b9f8e70063e3ca2c1c03bdb172496e579980
dav1d-sv1d <67202968+dav1d-sv1d@users.noreply.github.com>
2021-07-06 12:33:54 +0300

    Update test_bot.py

--- test_bot.py
+++ test_bot.py
@@ -12,6 +12,8 @@ from aiogram import types
 import os
 from dotenv import load_dotenv
 
+from aiogram.types import ContentType
+
 load_dotenv()
 
 bot_token = str(os.getenv("BOT_TOKEN"))
@@ -41,8 +43,6 @@ async def send_message(message: types.Message):
         amount=amount
     )
 
-    TIME_MACHINE_IMAGE_URL = ''
-
 
     await bot.send_invoice(
         message.chat.id,
@@ -66,7 +66,7 @@ async def send_message(message: types.Message):
 
 
 
-@dp.pre_checkout_query_handler(lambda query: True)
+@dp.pre_checkout_query_handler()
 async def process_pre_checkout_query(pre_checkout_query: types.PreCheckoutQuery):
 
     await bot.answer_pre_checkout_query(
@@ -74,9 +74,17 @@ async def process_pre_checkout_query(pre_checkout_query: types.PreCheckoutQuery)
         ok=True
     )
 
+
+@dp.message_handler(content_types=ContentType.SUCCESSFUL_PAYMENT)
+async def process_successful_payment(message: types.Message):
+
     time = datetime.datetime.now()
 
-    data = pre_checkout_query.order_info
+    pmnt = message.successful_payment.to_python()
+    for key in pmnt.keys():
+        print(f"{key}: {pmnt[key]}")
+
+    data = pmnt["order_info"]
 
     data_and_time = []
     info = []
@@ -94,23 +102,20 @@ async def process_pre_checkout_query(pre_checkout_query: types.PreCheckoutQuery)
     }
 
 
-    for item in data.iter_keys():
+    for item in data.keys():
         info.append(f"<b>{replace[item]}</b>: {data[item]}")
+
+    info.append("Ник в телеграмме: " f"<a href='tg://user?id={message.from_user.id}'>{message.from_user.full_name}</a>")
 
 
     await bot.send_message(
         chat_id=-1001598180481,
         text="<b>Название</b>: {}\n\n{}\n\n{}\n\n<b>Сумма оплаты</b>: {},{} грн.".format(
-            pre_checkout_query.invoice_payload,
+            pmnt["invoice_payload"],
             "\n".join(data_and_time),
             "\n".join(info),
-            str(pre_checkout_query["total_amount"])[:-2],
-            str(pre_checkout_query["total_amount"])[-2:]
+            str(pmnt["total_amount"])[:-2],
+            str(pmnt["total_amount"])[-2:]
         )
     )
 
-
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
